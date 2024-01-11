dd718e1a03e89580923844d0ab2bd7e0cbb70b55
Neucrack <CZD666666@gmail.com>
2024-01-11 18:03:03 +0800

    update readme, add pretty arg

--- pc_monitor_server/main.py
+++ pc_monitor_server/main.py
@@ -13,11 +13,12 @@ except Exception:
 
 app = Flask("pc_monitor_server")
 util = Util()
+pretty = False
 
 @app.route("/", methods=["POST", "GET"])
 def home():
     info = util.all()
-    return Response(json.dumps(info, ensure_ascii=False, indent = 4), mimetype="application/json")
+    return Response(json.dumps(info, ensure_ascii=False, indent = 4 if pretty else None), mimetype="application/json")
 
 def main(addr, port):
     app.run(addr, port= port)
@@ -45,11 +46,14 @@ def print_addr_qrcode(port):
         print(content)
 
 def main_cli():
+    global pretty
     parser = argparse.ArgumentParser()
     parser.add_argument("--addr", default="0.0.0.0", help="Address to listen on")
     parser.add_argument("-p", "--port", default=9999, help="Port to listen on")
+    parser.add_argument("--pretty", action="store_true", help="Pretty print for json(add indent)")
     args = parser.parse_args()
     print_addr_qrcode(args.port)
+    pretty = args.pretty
     main(args.addr, args.port)
 
 if __name__ == "__main__":
